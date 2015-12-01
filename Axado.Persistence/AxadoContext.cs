36eb1b8981f3075105e9cf0dd28ea6d8620e2fdc
Gabriel Lamounier <rezlam@live.com>
2015-11-30 23:36:54 -0200

    adiciona AxadoContext

---
+++ Axado.Persistence/AxadoContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Axado.Data.Entities;
+using Axado.Persistence.Mapping;
+
+namespace Axado.Persistence
+{
+    public class AxadoContext : DbContext
+    {
+        public AxadoContext()
+            : base("axado")
+        {
+            //
+        }
+    }
+}
---
+++ Axado.Persistence/Mapping/BaseEntityMap.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Infrastructure.Annotations;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Axado.Data.Entities;
+
+namespace Axado.Persistence.Mapping
+{
+    public abstract class BaseEntityMap<TEntity> : EntityTypeConfiguration<TEntity> where TEntity : BaseEntity
+    {
+        public BaseEntityMap()
+        {
+            // Primary key
+            this.HasKey(t => t.Id);
+
+            // Colunas padrão
+            this.Property(t => t.Id)
+                .HasColumnName("id");
+
+            this.Property(t => t.Active)
+                .HasColumnName("active")
+                .IsRequired();
+
+            this.Property(t => t.CreatedAt)
+                .HasColumnName("created_at")
+                .HasColumnType("datetime2")
+                .HasPrecision(3)
+                .IsRequired();
+
+            this.Property(t => t.UpdatedAt)
+                .HasColumnName("updated_at")
+                .HasColumnType("datetime2")
+                .HasPrecision(3)
+                .IsOptional();
+
+            this.Property(t => t.DeletedAt)
+                .HasColumnName("deleted_at")
+                .HasColumnType("datetime2")
+                .HasPrecision(3)
+                .IsOptional();
+        }
+    }
+}
